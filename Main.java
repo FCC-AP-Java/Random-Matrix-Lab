06361ad14701696b957c3ead1dae1213ddef7f94
MercuriusX <trianglehairline@gmail.com>
2021-02-22 19:11:31 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
+import java.util.*;
+
 class Main {
   public static void main(String[] args) 
   {
-    
+    RandomMatrix matrix1 = new RandomMatrix(5,2,60);
+
+    System.out.println("Matrix of values:");
+    matrix1.print();
+
+    System.out.println("Count of given value: " + matrix1.getAmount(4) + "\n");
+
+    System.out.println("Array of sums of rows: " + Arrays.toString(matrix1.getRowTotals()) + "\n");
+
+    System.out.println();
+    System.out.println("Array of sums of columns: " + Arrays.toString(matrix1.getColumnTotals()) + "\n");
+
+    System.out.println("Total of all values: " + matrix1.getTotal() + "\n");
+
+    System.out.println("Are all values unique?: " + matrix1.allValuesUnique());
   }
 }
---
+++ RandomMatrix.java
@@ -0,0 +1,105 @@
+import java.util.*;
+
+public class RandomMatrix
+{
+  private int upperBound;
+  private int[][] matrix;
+
+  public RandomMatrix(int rows, int cols, int max)
+  {
+    matrix = new int[rows][cols];
+    upperBound = max;
+
+    for (int i = 0; i < matrix.length; i++)
+    {
+      for (int j = 0; j < matrix[0].length; j++)
+      {
+        matrix[i][j] = (int)(Math.random() * upperBound);
+      }
+    }
+  }
+
+  public void print()
+  {
+    for (int[] row : matrix)
+    {
+      System.out.println(Arrays.toString(row) + "\n");
+    }
+  }
+
+  public int getAmount(int given)
+  {
+    int counter = 0;
+    for (int[] row : matrix)
+    {
+      for (int value : row)
+      {
+        if (value == given)
+        {
+          counter++;
+        }
+      }
+    }
+    return counter;
+  }
+
+  public int[] getRowTotals()
+  {
+    int[] rowTotals = new int[matrix.length];
+    for (int i = 0; i < matrix.length; i++)
+    {
+      for (int j = 0; j < matrix[0].length; j++)
+      {
+        rowTotals[i] += matrix[i][j];
+      }
+    }
+    return rowTotals;
+  }
+
+  public int[] getColumnTotals()
+  {
+    int[] columnTotals = new int[matrix[0].length];
+    for (int i = 0; i < matrix.length; i++)
+    {
+      for (int j = 0; j < matrix[0].length; j++)
+      {
+        columnTotals[j] += matrix[i][j];
+      }
+    }
+    return columnTotals;
+  }
+
+  public int getTotal()
+  {
+    int sum = 0;
+    for (int[] row : matrix)
+    {
+      for (int value : row)
+      {
+        sum += value;
+      }
+    }
+    return sum;
+  }
+
+  public boolean allValuesUnique()
+  {
+    ArrayList<Integer> convertedList = new ArrayList<Integer>();
+    for (int[] row : matrix)
+    {
+      for (int value : row)
+      {
+        convertedList.add(value);
+      }
+    }
+    Collections.sort(convertedList);
+    for (int i = 1; i < convertedList.size(); i++)
+    {
+      if (convertedList.get(i) == convertedList.get(i - 1))
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+}
